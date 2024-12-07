6d1449a4a302230d9181f53f6da8122581022eb1
ccasal <christian.casal@acats.com>
2024-12-06 22:16:39 -0500

    day 1, p1

--- aoc-python/2024/1/part1.py
+++ aoc-python/2024/1/part1.py
@@ -1,1 +1,26 @@
-print('o hai world!')
+import math
+
+inputfile = open('input.txt', 'r')
+
+firstSet = []
+secondSet = []
+
+for line in inputfile.readlines():
+    firstNumber = int(line.split('   ')[0].strip())
+    secondNumber = int(line.split('   ')[1].strip())
+    firstSet.append(firstNumber)
+    secondSet.append(secondNumber)
+
+
+firstSet = sorted(firstSet)
+secondSet = sorted(secondSet)
+
+totalDistance = 0
+for i in range(len(firstSet)):
+    x = firstSet[i]
+    y = secondSet[i]
+    setDistance = math.dist([x], [y])
+    totalDistance += setDistance
+    print(setDistance)
+
+print(totalDistance)
